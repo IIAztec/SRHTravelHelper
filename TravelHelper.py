abd7a3fab5d53dc95b00d09a937b768556c56e36
Ignat Danilin <danilin.ignat@gmail.com>
2026-05-18 14:10:14 +0200

    Add SQLite database setup for flights and trains

--- TravelHelper.py
+++ TravelHelper.py
@@ -1,1 +1,21 @@
+import sqlite3
+conn = sqlite3.connect("database.db")
+cursor = conn.cursor()
 
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS flights (
+    flight_number INTEGER PRIMARY KEY AUTOINCREMENT,
+    dep_air TEXT,
+    arr_air TEXT,
+    flight_date TEXT
+    )
+''')
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS trains (
+    train_number INTEGER PRIMARY KEY AUTOINCREMENT,
+    train_station TEXT,
+    train_date TEXT
+    )
+''')
+conn.commit()
+conn.close()
