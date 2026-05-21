a4c67d9b3cc002ff6f0b7549473d417f8dae6fc9
Konstantin Gavrilov <gavrilovkd1@gmail.com>
2026-05-21 15:52:07 +0200

    Implement hotel database creation and search results insertion
    Added functionality to create a hotels table and insert search results into it.

--- TravelHelper.py
+++ TravelHelper.py
@@ -19,3 +19,43 @@ cursor.execute('''
 ''')
 conn.commit()
 conn.close()
+
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS hotels (
+    token TEXT PRIMARY KEY, 
+    name TEXT, 
+    latitude REAL,
+    longitude REAL,
+    check_in_time TEXT,
+    check_out_time TEXT,
+    rent_per_night REAL
+    )
+    ''')
+conn.commit()
+conn.close()
+
+def find_accommodation(destination, check_in, check_out):
+    hotel_search_results = client.search({
+      "engine": "google_hotels",
+      "q": destination,
+      "check_in_date": check_in,
+      "check_out_date": check_out,
+    })
+    properties = hotel_search_results["properties"]
+    for i in properties:
+        cursor.execute('''
+        INSERT INTO hotels (token, name, latitude, longitude, check_in, check_out, per_night)
+        VALUES(?, ?, ?, ?, ?, ?, ?)'''), i["property_token"], i["name"], i["gps_coordinates"]["latitude"], i["gps_coordinates"]["longitude"], i["check_in_time"], i["check_out_time"], i["rent_per_night"]["extracted_lowest"]
+        cursor.commit()
+        cursor.close()
+
+def show_accommodation(destination, check_in, check_out):
+    cursor.executemany('''
+        SELECT * FROM hotels
+                    ''')
+    records = cursor.fetchall()
+    cursor.close()
+    print("Name | smth")
+    for record in records:
+        print(record)
