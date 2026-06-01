fca9d0f81a08707b5255d10d2d7e8c3f5633301a
Ignat Danilin <danilin.ignat@gmail.com>
2026-06-01 13:56:17 +0200

    Add files via upload

--- system.py
+++ system.py
@@ -1,6 +1,7 @@
 import sqlite3
 import requests
 import serpapi
+import random
 from datetime import datetime
 conn = sqlite3.connect("data.db")
 client = serpapi.Client(api_key="5386b5af7eda19abbe487a89b5e8dbb3a3504763f8691e718f505eed31640c03")
@@ -8,31 +9,22 @@ cursor = conn.cursor()
 
 cursor.execute('''
     CREATE TABLE IF NOT EXISTS flights (
-    flight_ID TEXT PRIMARY KEY,
+    flight_ID TEXT,
+    request_date TEXT,
     flight_date TEXT,
     flight_company TEXT,
     dep_air TEXT,
     arr_air TEXT,
-    booking_token
-    )
-''')
-cursor.execute('''
-    CREATE TABLE IF NOT EXISTS trains (
-    train_number INTEGER PRIMARY KEY AUTOINCREMENT,
-    train_station TEXT,
-    train_date TEXT
-    )
-''')
-cursor.execute('''
-    CREATE TABLE IF NOT EXISTS buses (
-    bus_number INTEGER PRIMARY KEY AUTOINCREMENT,
-    bus_station TEXT,
-    bus_date TEXT
+    layover_air TEXT,
+    priceEUR TEXT,
+    primary_token TEXT PRIMARY KEY
     )
 ''')
 cursor.execute('''
     CREATE TABLE IF NOT EXISTS hotelist (
     hotelToken TEXT PRIMARY KEY,
+    hotelDestination TEXT,
+    request_date TEXT,
     hotelName TEXT,
     timeOfCheckIn TEXT,
     timeOfCheckOut TEXT,
@@ -45,33 +37,31 @@ cursor.execute('''
 
 conn.commit()
 
-flightBase = ""
-transferBase = ""
-hotelBase = ""
-
-def get_json(url, params=None, headers=None):
-    response = requests.get(
-        url,
-        params=params,
-        headers=headers,
-        timeout=30 # fail fast if API is unresponsive
-    )
-    response.raise_for_status() 
-    return response.json()
-
-def getAPIData(base):
-    url = f"{base}.json"
-    data = get_json(url, params={"limit": 100})
-    finaldata = data
-    return finaldata
-
-def myTextFormat(text, length=20):
+def myTextFormat(text, length=20): # Kostya
     if len(text) > length:
         return text[:length-3] + "..."
     else:
         return text + " " * (length - len(text))
 
-def searchInDBHotels(destination, check_in, check_out):
+def cityToCode(city): # Kostya
+    with open("airport-codes-new.txt", "r", encoding="utf-8") as f:
+        airport_codes = f.readlines()
+    for line in airport_codes:
+        if line.split(",")[0] == city:
+            return line.split(",")[2]
+        if city in line:
+            return line.split(",")[2]
+    return None
+
+def checkCity(city): # Kostya
+    with open("airport-codes-new.txt", "r", encoding="utf-8") as f:
+        airport_codes = f.readlines()
+    for line in airport_codes:
+        if line.split(',')[0] == city:
+            return True
+    return False
+
+def searchInDBHotels(destination, check_in, check_out): # Kostya
     cursor.execute('''
         SELECT hotelToken FROM hotelist WHERE hotelDestination = ? AND request_date = ?
                     ''', (destination, datetime.today().strftime("%Y-%m-%d")))
@@ -84,7 +74,159 @@ def searchInDBHotels(destination, check_in, check_out):
             index += 1
     return hotelTokens
 
-def find_accommodation(destination, check_in, check_out):
+def currentDate():
+    today = datetime.today()
+    formatedDate = today.strftime("%Y-%m-%d")
+    return formatedDate
+
+def checkSameDateForFlights():
+    cursor.execute("SELECT request_date, flight_date, dep_air, arr_air FROM flights")
+    exists_data = cursor.fetchall()
+    conn.commit()
+    return exists_data
+
+def printAvalibleTickets():
+    cursor.execute("SELECT flight_date, dep_air, arr_air, layover_air, priceEUR FROM flights")
+    exists_data = cursor.fetchall()
+    conn.commit()
+    for i in exists_data:
+        print(f"Date of departue: {i[0]}")
+        print(f"Departue airport: {i[1]}")
+        print(f"Arrival airport: {i[2]}")
+        print(f"List of layover airports: {i[3]}")
+        print(f"Price in EUR: {i[4]}")
+
+def show_accommodation(destination, check_in, check_out): # Kostya
+    cursor.execute('''SELECT * FROM hotelist
+                    WHERE hotelDestination = ? AND hotelToken LIKE ? AND request_date = ?''', (destination, f"%{check_in}{check_out}%", datetime.today().strftime("%Y-%m-%d")))
+    records = cursor.fetchall()
+    print("________Name________ | Latitude | Longitude | Check-in | Check-out | Price for one night")
+    for record in records:
+        print(myTextFormat(record[3]), "|", myTextFormat(str(record[6]), 8), "|", myTextFormat(str(record[7]), 9), "|", myTextFormat(record[4], 8), "|", myTextFormat(record[5], 9), "|", myTextFormat(str(record[8]), 6))
+
+
+# def requestAirports():
+#     depAir = input("Enter departue airport(CODE): ")
+#     arrAir = input("Enter arriving airport(CODE): ")
+#     valid_date = False
+#     while valid_date == False:
+#         print("--------")
+#         right_format = "%Y-%m-%d"
+#         today = datetime.today()
+#         formatedDate = today.strftime("%Y-%m-%d")
+#         print(f"Current date: {formatedDate}")
+#         dateOfDep = input("Enter date when you wish to fly(YYYY-MM-DD): ")
+#         try:
+#             if datetime.strptime(dateOfDep, right_format):
+#                 valid_date = True
+#                 print("The system is searching for ticket")
+#                 break
+#         except ValueError:
+#             print("Wrong date. Try again")
+#     list_of_data = {"depAir": depAir, "arrAir": arrAir, "dateOfDep": dateOfDep}
+#     return list_of_data
+
+def genInput(tr_type = "ret"):
+    depAir = input("Enter departure city: ")
+    while checkCity(depAir) == False:
+        print("City not found. Please try again.")
+        depAir = input("Enter departure city: ")
+    arrAir = input("Enter arrival city: ")
+    while checkCity(arrAir) == False:
+        print("City not found. Please try again.")
+        arrAir = input("Enter arrival city: ")
+    valid_date = False
+    while valid_date == False:
+        print("--------")
+        right_format = "%Y-%m-%d"
+        today = datetime.today()
+        print("Today's date is:", today.strftime(right_format))    
+        dateOfDep = input("Enter date of departure (YYYY-MM-DD): ")
+        try:
+            dep_date = datetime.strptime(dateOfDep, right_format)
+            if dep_date < today:
+                print("Date cannot be in the past. Please try again.")
+            else:
+                valid_date = True
+        except ValueError:
+            print("Invalid date format. Please try again.")
+    valid_date = False
+    while valid_date == False:
+        print("--------")
+        right_format = "%Y-%m-%d"
+        today = datetime.today()
+        print("Today's date is:", today.strftime(right_format))    
+        dateOfReturn = input("Enter date of return (YYYY-MM-DD): ")
+        try:
+            ret_date = datetime.strptime(dateOfReturn, right_format)
+            if ret_date < today:
+                print("Date cannot be in the past. Please try again.")
+            else:
+                valid_date = True
+        except ValueError:
+            print("Invalid date format. Please try again.")
+    return {"depAir": depAir, "arrAir": arrAir, "dateOfDep": dateOfDep, "dateOfReturn": dateOfReturn}
+
+def loadDataToBase(flights_list):
+    for j in range(len(flights_list)):
+        try:
+            flight = flights_list[j]["flights"][0]
+            listoftick = (
+                flight["flight_number"],
+                currentDate(),
+                flight["departure_airport"]["time"][:10],
+                flight["airline"],
+                flight["departure_airport"]["id"],
+                flights_list[j]["flights"][-1]["arrival_airport"]["id"],
+                ",".join([i["id"] for i in flights_list[j]["layovers"]]),
+                int(flights_list[j]["price"]),
+                str(random.random()),
+            )
+        except KeyError:
+            flight = flights_list[j]["flights"][0]
+            listoftick = (
+                flight["flight_number"],
+                currentDate(),
+                flight["departure_airport"]["time"][:10],
+                flight["airline"],
+                flight["departure_airport"]["id"],
+                flights_list[j]["flights"][-1]["arrival_airport"]["id"],
+                "0",
+                int(flights_list[j]["price"]),
+                str(random.random()),
+            )
+        cursor.execute("INSERT INTO flights (flight_ID, request_date, flight_date, flight_company, dep_air, arr_air, layover_air, priceEUR, primary_token) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", listoftick)
+        conn.commit()
+        
+
+def requestAirAPI(dep, arr, date):
+    results = client.search({
+    "engine": "google_flights",
+    "departure_id": dep,
+    "arrival_id": arr,
+    "currency": "EUR",
+    "type": "2",
+    "outbound_date": date
+    })
+    best_flights = results["best_flights"]
+    return best_flights
+
+def findOneWayFlights(data):
+    exists_data = checkSameDateForFlights()
+    if exists_data != []:
+        for i in exists_data:
+            if not (i[0] == currentDate() and i[1] == data["dateOfDep"] and i[2] == data["depAir"] and i[3] == data["arrAir"]):
+                tickets = requestAirAPI(cityToCode(data["depAir"]), cityToCode(data["arrAir"]), data["dateOfDep"])
+                loadDataToBase(tickets)
+                printAvalibleTickets()
+            else:
+                print("AMOGUS")
+    else:
+        tickets = requestAirAPI(cityToCode(data["depAir"]), cityToCode(data["arrAir"]), data["dateOfDep"])
+        loadDataToBase(tickets)
+        printAvalibleTickets()
+
+def find_accommodation(destination, check_in, check_out): # Kostya
     hotel_search_results = client.search({
         "engine": "google_hotels",
         "q": destination,
@@ -102,66 +244,44 @@ def find_accommodation(destination, check_in, check_out):
         except KeyError:
             continue
 
-def show_accommodation(destination, check_in, check_out):
-    cursor.execute('''SELECT * FROM hotelist
-                    WHERE hotelDestination = ? AND hotelToken LIKE ? AND request_date = ?''', (destination, f"%{check_in}{check_out}%", datetime.today().strftime("%Y-%m-%d")))
-    records = cursor.fetchall()
-    print("________Name________ | Latitude | Longitude | Check-in | Check-out | Price for one night")
-    for record in records:
-        print(myTextFormat(record[3]), "|", myTextFormat(str(record[6]), 8), "|", myTextFormat(str(record[7]), 9), "|", myTextFormat(record[4], 8), "|", myTextFormat(record[5], 9), "|", myTextFormat(str(record[8]), 6))
-
-def requestAirports():
-    depAir = input("Enter departue airport(CODE): ")
-    arrAir = input("Enter arriving airport(CODE): ")
-    valid_date = False
-    while valid_date == False:
-        print("--------")
-        right_format = "%Y-%m-%d"
-        today = datetime.today()
-        formatedDate = today.strftime("%Y-%m-%d")
-        print(f"Current date: {formatedDate}")
-        dateOfDep = input("Enter date when you wish to fly(YYYY-MM-DD): ")
-        try:
-            if datetime.strptime(dateOfDep, right_format):
-                valid_date = True
-                print("The system is searching for ticket")
+def menu():
+    while True:
+        print("1. Search for a one-way flight")
+        print("2. Search for hotels")
+        print("3. Search for a round-trip flight and a hotel")
+        print("4. Exit")
+        choice = input("Please enter your choice (1-4): ")
+        if choice>="1" and choice<"4":
+            tr_dat = genInput("ret" if choice == "3" else "hotel_only" if choice == "2" else "one way")
+        match choice:
+            case "1":
+                findOneWayFlights(tr_dat)
+            case "2":
+                destination = tr_dat["arrAir"] 
+                check_in = tr_dat["dateOfDep"]
+                check_out = tr_dat["dateOfReturn"]
+                if searchInDBHotels(destination, check_in, check_out) == []:
+                    find_accommodation(destination, check_in, check_out)
+                    print("DEV: Hotel data has been saved to the database.")
+                else:
+                    print("DEV: Hotel data already exists in the database.")
+                show_accommodation(destination, check_in, check_out)
+            case "3":
+                destination = tr_dat["arrAir"] 
+                check_in = tr_dat["dateOfDep"]
+                check_out = tr_dat["dateOfReturn"] 
+                findOneWayFlights(tr_dat)
+                findOneWayFlights({"depAir": tr_dat["arrAir"], "arrAir" : tr_dat["depAir"], "dateOfDep":tr_dat["dateOfReturn"]})
+                if searchInDBHotels(destination, check_in, check_out) == []: # still not sure how to pass data from the flight fun to the hotels one
+                    find_accommodation(destination, check_in, check_out)
+                    print("DEV: Hotel data has been saved to the database.")
+                else:
+                    print("DEV: Hotel data already exists in the database.")
+                show_accommodation(destination, check_in, check_out)
+            case "4":
                 break
-        except ValueError:
-            print("Wrong date. Try again")
-    list_of_data = {"depair": depAir, "arrAir": arrAir, "dateOfDep": dateOfDep}
-    return list_of_data
+            case _:
+                print("Invalid choice. Please try again.")
+    conn.close()
 
-def loadDataToBase(flights_list):
-    flight = flights_list[0]["flights"][0]
-    listoftick = (
-        flight["flight_number"],
-        flight["departure_airport"]["time"][:10],
-        flight["airline"],
-        flight["departure_airport"]["id"],
-        flight["arrival_airport"]["id"],
-    )
-    cursor.execute("INSERT INTO flights (flight_ID, flight_date, flight_company, dep_air, arr_air) VALUES (?, ?, ?, ?, ?)", listoftick)
-    conn.commit()
-
-def requestAirAPI(dep, arr, date):
-    results = client.search({
-    "engine": "google_flights",
-    "departure_id": dep,
-    "arrival_id": arr,
-    "currency": "EUR",
-    "type": "2",
-    "outbound_date": date
-    })
-    best_flights = results["best_flights"]
-    return best_flights
-
-
-# def printAvailibleflights(json):
-    
-
-
-print("Welcome to JOPA")
-data = requestAirports()
-tickets = requestAirAPI(data["depair"], data["arrAir"], data["dateOfDep"])
-loadDataToBase(tickets)
-print(tickets)
+menu()
